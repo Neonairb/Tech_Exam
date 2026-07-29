6572881fd082040d520a2fec76a87021a7908695
Neonairb <brajai@hotmail.com>
2026-07-29 10:35:44 -0600

    add crear y actualizar empleados Dtos

---
+++ backend/TechExamBackend/TechExamBackend/Dtos/ActualizarEmpleadoRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechExamBackend.Dtos
+{
+    public class ActualizarEmpleadoRequest
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(
+            50,
+            MinimumLength = 1,
+            ErrorMessage = "El nombre debe tener entre 1 y 50 caracteres."
+        )]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
---
+++ backend/TechExamBackend/TechExamBackend/Dtos/CrearEmpleadoRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TechExamBackend.Dtos
+{
+    public class CrearEmpleadoRequest
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "El identificador debe ser mayor que cero.")]
+        public int IdEmpleado { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
+        [StringLength(
+            50,
+            MinimumLength = 1,
+            ErrorMessage = "El nombre debe tener entre 1 y 50 caracteres."
+        )]
+        public string Nombre { get; set; } = string.Empty;
+    }
+}
